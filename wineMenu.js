0b0cb8d3dc6191ccde61fb6d9936c962e8ef4a51
Ashley Schuett <ashleyns1992@gmail.com>
2016-06-01 20:02:50 -0400

    fixed spacing for readabilty

--- wineMenu.js
+++ wineMenu.js
@@ -9,10 +9,9 @@ exports.createWineMenu = (wines, restaurant_name, descriptions) => {
 
 var fillTemplate = (wines, restaurant_name, descriptions) => {
 
-  let template = descriptions ? "/wine-menu-template_2.docx" : "/wine-menu-template-no-description.docx";
+	let template = descriptions ? "/wine-menu-template_2.docx" : "/wine-menu-template-no-description.docx";
 
-	var content = fs
-    .readFileSync(__dirname + template, "binary");
+	var content = fs.readFileSync(__dirname + template, "binary");
 
 	var doc = new Docxtemplater(content);
 
@@ -48,17 +47,17 @@ var sortWineByType = wines => {
 
 
 	return [{
-				type : 'White',
-				varietals : sortPrice(white)
-			},
-			{
-				type : 'Blush',
-				varietals : sortPrice(blush)
-			},
-			{
-				type : 'Red',
-				varietals : sortPrice(red)
-			}];
+		type : 'White',
+		varietals : sortPrice(white)
+	},
+	{
+		type : 'Blush',
+		varietals : sortPrice(blush)
+	},
+	{
+		type : 'Red',
+		varietals : sortPrice(red)
+	}];
 
 }
 
@@ -72,7 +71,7 @@ var sortPrice = wines => {
 
 var sortWineByVarietal = wines => {
 	var filteredWines = [];
-  wines = wines.sort((a, b) => {return a.Varietal.specID-b.Varietal.specID});
+  	wines = wines.sort((a, b) => {return a.Varietal.specID-b.Varietal.specID});
 
 	let matchedGroup;
 	wines.forEach(wine => {
@@ -85,15 +84,16 @@ var sortWineByVarietal = wines => {
 			filteredWines.push(matchedGroup);
 		} else matchedGroup = match;
     
-		matchedGroup.vw.push({year : wine.item_year == 0 ? 'NV' : wine.item_year,
-									               producer : wine.Vineyard.name_text ? wine.Vineyard.name_text : '',
-									 	                 name : wine.item_name_desc ? '- '+wine.item_name_desc : '',
-							           wine_description : wine.item_description ? wine.item_description.replace(re, '') : '',
-                                      bin : wine.bin ? '[BIN '+ wine.bin +']' : '',
-							 	                       gp : wine.glass_price ? '$'+wine.glass_price : '',
-							 	                       ip : wine.item_price ? '$'+wine.item_price : '',
-                               item_price : wine.item_price});
-
+		matchedGroup.vw.push({
+			year : wine.item_year == 0 ? 'NV' : wine.item_year,
+			producer : wine.Vineyard.name_text ? wine.Vineyard.name_text : '',
+			name : wine.item_name_desc ? '- '+wine.item_name_desc : '',
+			wine_description : wine.item_description ? wine.item_description.replace(re, '') : '',
+                        bin : wine.bin ? '[BIN '+ wine.bin +']' : '',
+			gp : wine.glass_price ? '$'+wine.glass_price : '',
+			ip : wine.item_price ? '$'+wine.item_price : '',
+                        item_price : wine.item_price
+		});
 	});
 	return filteredWines;
 }
